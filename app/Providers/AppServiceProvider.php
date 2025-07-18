ff9e1a864bc8db13de6912881caa811e6319110b
allmight777 <agoliganange15@gmail.com>
2025-07-18 12:39:39 +0100

    Initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    use Illuminate\Routing\UrlGenerator;
+
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+# Lancer PHP-FPM et NGINX
+php-fpm -D
+nginx -g "daemon off;"
